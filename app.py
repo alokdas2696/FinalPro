9fc39b35f8b4edb48c46477e0795c865a9e2e07f
Your Name <you@example.com>
2022-07-26 04:35:02 +0530

    10th commit

--- app.py
+++ app.py
@@ -4,43 +4,22 @@ import os
 from flask import Flask, render_template, session,redirect,flash,make_response
 from flask_sqlalchemy import SQLAlchemy, request
 import random
-import smtplib
 from email.message import EmailMessage
 from flask_mail import *
-import json
 import pdfkit
-path_wkthmltopdf = b"C:\Program Files\wkhtmltopdf\\bin\wkhtmltopdf.exe"
-config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
 
 
-# from random import randint
 app = Flask(__name__)
 app.secret_key = "login"
-
-
-# with open('config.json', 'r') as k:
-#     params = json.load(k)['params']
-# otp = randint(1111, 9999)
-
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/studb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-# app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-# app.config['MAIL_PORT'] = 465
-# app.config['MAIL_USERNAME'] = params['gmail-user']
-# app.config['MAIL_PASSWORD'] = params['gmail-password']
-# app.config['MAIL_USERNAME'] = ['gmail-user']
-# app.config['MAIL_PASSWORD'] = ['gmail-password']
-# app.config['MAIL_USE_TLS'] = False
-# app.config['MAIL_USE_SSL'] = True
-
-mail = Mail(app)
+# mail = Mail(app)
 
 
-def generateOtp():
+def generate_otp():
     return random.randint(1111, 9999)
 
 class Student(db.Model):
@@ -56,48 +35,34 @@ class Student(db.Model):
         return f"Student('{self.stuid}','{self.name}','{self.email}','{self.mbno}','{self.mtmarks}','{self.scmarks}','{self.csmarks}')"
 
 
-
-def sendOtp(email, x):
-    msg = EmailMessage()
-    msg['Subject'] = 'OTP FROM Alok Server'
-    msg['From'] = 'alokdas9626@gmail.com'
-    msg['To'] = email
-    # x = generateOtp()
-    msg.set_content(str(x))
-    session['response'] = str(x)  # Storing otp in session
-
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login('alokdas9626@gmail.com','keoqvilaqresvgvj')
-        smtp.send_message(msg)
-
-
 @app.route('/', methods=['GET'])
 def home():
     session.pop('username', None)
     return render_template("email.html")
 
 
-# @app.route('/verify', methods=['GET', 'POST'])
-# def verify():
-#     if request.method == 'POST':
-#         # email = request.form.get('email')
-#
-#         d = request.form.get('rollno')
-#         stuid = Student.query.get(d)
-#         if stuid == None:
-#             return "Invalid Roll number ... go back"
-#         email = stuid.email
-#         # if email.strip() == email:
-#
-#
-#         if email.strip() == email:
-#             msg = Message('OTP',sender='alokdas9626@gmail.com', recipients=[email])
-#             msg.body = str(otp)
-#             mail.send(msg)
-#             return render_template("verify.html", d=d)
-#         else:
-#             return " Unmatched EmailID with Roll No"
-#     return render_template('verify.html')
+def send_otp(email, x):
+    x = generate_otp()
+    session['response'] = str(x)  # Storing otp in session
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login('alokdas9626@gmail.com', 'keoqvilaqresvgvj')
+        subject = 'OTP FROM RESULT SERVER'
+        body = x
+        msg = f'Subject:{subject}\n\nDear Student \n \n The one-time password (OTP) for validating your email id is :-     {body} and do not share this OTP with anyone.'
+        smtp.sendmail('alokdas9626@gmail,com', email, msg)
+
+
+@app.route("/resendotp", methods=['POST'])
+def resend():
+    if request.method == 'POST':
+        if 'email' in session:
+            emaildb = session['email'][0]
+            x = generate_otp()  # generate otp
+            send_otp(emaildb, x)  # resend otp
+            d = session['email'][1]
+            return render_template("verify.html", d=d)
+        return render_template("email.html")
+
 
 @app.route("/verify", methods=['GET','POST'])
 def verify():
@@ -109,26 +74,14 @@ def verify():
 
         emaildb = stuid.email
         session['email'] = (emaildb, d)    #will store this in session for resend otp
-        x = generateOtp()
-        sendOtp(emaildb, x)
+        x = generate_otp()
+        send_otp(emaildb, x)
 
         return render_template("verify.html", d=d)
     return render_template("email.html")
 
 
-@app.route("/resendotp", methods=['POST'])
-def resend():
-    if request.method == 'POST':
-        if 'email' in session:  # resend otp
-            emaildb = session['email'][0]  # resend otp
-            x = generateOtp()  # resend otp
-            sendOtp(emaildb, x)  # resend otp
-            d = session['email'][1]
-            return render_template("verify.html", d=d)
-        return render_template("email.html")
-
-
-@app.route('/validate/<int:d>',methods=['GET', 'POST'])
+@app.route('/validate/<int:d>', methods=['GET', 'POST'])
 def validate(d):
     if request.method == 'POST':
         f = Student.query.get(d)
@@ -138,38 +91,30 @@ def validate(d):
             s = session['response']
             session.pop('response', None)
             if s == str(userotp):
-                # if otp == int(userotp):
-                msg = EmailMessage()
-                msg['Subject'] = 'Mail from Alok server'
-                msg['From'] = 'alokdas9626@gmail.com'
-                msg['To'] = emaildb
 
                 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
                     smtp.login('alokdas9626@gmail.com', 'keoqvilaqresvgvj')
-                    # subject = 'Res'
+                    subject = 'Result'
                     body = (str(f.name) + "\n Email: " + str(f.email) + "\n Math Marks: " + str(f.mtmarks) + "\n Science Marks: " + str(f.scmarks)+ "\n Computer Marks: " + str(f.csmarks)+ "\n Total Marks:" + str(f.csmarks + f.mtmarks + f.scmarks)+ "\n Percenatge: " + str(((f.csmarks + f.mtmarks + f.scmarks)/300)*100))
 
-                    msg = f'{body}'
+                    msg = f'Subject:{subject}\n\n{body}'
                     smtp.sendmail('alokdas9626@gmail,com', emaildb, msg)
                 return render_template('result.html', f=f, d=d, msg="Result has been Send to your given respected Email Id")
-        return render_template("email.html", msg="Otp not verified .... Wrong OTP!")
-        # return render_template('email.html', msg="Session Expired (Otp Already Used) Try again !")
+        return render_template("email.html", msg="Wrong OTP!........Please Login Again")
+        # return render_template('email.html')
 
 
-# @app.route('/validate/<int:d>',methods=['GET', 'POST'])
-# def validate(d):
-#     if request.method == 'POST':
-#         f = Student.query.get(d)
-#         gmail = f.email
-#         userotp = request.form['otp']
-#         if otp == int(userotp):
-#             mail.send_message("Result: ",sender='alokdas9626@gmail.com', recipients=[gmail], body=f.name + "\n Email: " + f.email + "\n Math Marks: " + str(f.mtmarks)
-#                               + "\n Science Marks: " + str(f.scmarks)+ "\n Computer Marks: " + str(f.csmarks)+ "\n Total Marks:"
-#                             + str(f.csmarks + f.mtmarks + f.scmarks)+ "\n Percenatge: " +str(round(((f.csmarks + f.mtmarks + f.scmarks)/300)*100)))
-#
-#             return render_template('result.html',f=f, d=d, msg="Result has been Send to your given respected Email Id")
-#         return render_template('email.html', msg="Not Verified !! try again")
-
+@app.route('/download/<int:d>')
+def download(d):
+    f = Student.query.get(d)
+    rendered = render_template('result.html', f=f, msg="Result has been Send to your given respected Email Id")
+    path_wkthmltopdf = b"C:\Program Files\wkhtmltopdf\\bin\wkhtmltopdf.exe"
+    config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
+    pdf = pdfkit.from_string(rendered, False, configuration=config)
+    response = make_response(pdf)
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = 'attachment; filename=MyResult.pdf'
+    return response
 
 
 @app.route("/login", methods=['GET','POST'])
@@ -206,7 +151,6 @@ def add():
 
             alldata = Student.query.all()
             for student in alldata:
-
                 if student.email == email:
                     flash(f"This data already exits: {email} ", "info")
                     return redirect('/admin')
@@ -272,6 +216,7 @@ def delete(stuid):
     else:
         return redirect("/login")
 
+
 @app.route('/search',methods=['GET','POST'])
 def search():
     if 'username' in session:
@@ -281,19 +226,7 @@ def search():
             page = request.args.get('page', 1, type=int)
             alldata1 = Student.query.filter(Student.name.like(search)).paginate(per_page=20, page=int(page))
             return render_template('index1.html', alldata1=alldata1, tag=tag)
-
         return render_template("login.html")
-
-
-@app.route('/download/<int:d>')
-def download(d):
-    f = Student.query.get(d)
-    rendered = render_template('result.html', f=f, msg="Result has been Send to your given respected Email Id")
-    pdf = pdfkit.from_string(rendered, False, configuration=config)
-    response = make_response(pdf)
-    response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Content-Disposition'] = 'attachment; filename=MyResult.pdf'
-    return response
 
 
 if __name__ == "__main__":

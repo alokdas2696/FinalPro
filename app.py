d65d7bf9e9dbccf640e7bb2cdc9fd48c3086d057
Your Name <you@example.com>
2022-07-27 16:48:20 +0530

    10th commit

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 
 import os
 
-from flask import Flask, render_template, session,redirect,flash,make_response
+from flask import Flask, render_template, session, redirect, flash, make_response
 from flask_sqlalchemy import SQLAlchemy, request
 import random
-from email.message import EmailMessage
 from flask_mail import *
 import pdfkit
 
@@ -16,11 +15,11 @@ app.secret_key = "login"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/studb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-# mail = Mail(app)
 
 
 def generate_otp():
     return random.randint(1111, 9999)
+
 
 class Student(db.Model):
     stuid = db.Column(db.Integer(), unique=True, primary_key=True)
@@ -48,11 +47,13 @@ def send_otp(email, x):
         smtp.login('alokdas9626@gmail.com', 'keoqvilaqresvgvj')
         subject = 'OTP FROM RESULT SERVER'
         body = x
-        msg = f'Subject:{subject}\n\nDear Student \n \n The one-time password (OTP) for validating your email id is :-     {body} and do not share this OTP with anyone.'
+        msg = f'Subject:{subject}\n\nDear Student \n \n ' \
+              f'The one-time password (OTP) for validating your email id is :-{body} ' \
+              f'and do not share this OTP with anyone.'
         smtp.sendmail('alokdas9626@gmail,com', email, msg)
 
 
-@app.route("/resendotp", methods=['POST'])
+@app.route("/resendotp", methods=['GET','POST'])
 def resend():
     if request.method == 'POST':
         if 'email' in session:
@@ -61,7 +62,7 @@ def resend():
             send_otp(emaildb, x)  # resend otp
             d = session['email'][1]
             return render_template("verify.html", d=d)
-        return render_template("email.html")
+    return redirect('/')
 
 
 @app.route("/verify", methods=['GET','POST'])
@@ -95,16 +96,20 @@ def validate(d):
                 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
                     smtp.login('alokdas9626@gmail.com', 'keoqvilaqresvgvj')
                     subject = 'Result'
-                    body = (str(f.name) + "\n Email: " + str(f.email) + "\n Math Marks: " + str(f.mtmarks) + "\n Science Marks: " + str(f.scmarks)+ "\n Computer Marks: " + str(f.csmarks)+ "\n Total Marks:" + str(f.csmarks + f.mtmarks + f.scmarks)+ "\n Percenatge: " + str(((f.csmarks + f.mtmarks + f.scmarks)/300)*100))
+                    body = (str(f.name) + "\n Email: " + str(f.email) + "\n Math Marks: " + str(f.mtmarks) +
+                            "\n Science Marks: " + str(f.scmarks)+ "\n Computer Marks: " + str(f.csmarks) +
+                            "\n Total Marks:" + str(f.csmarks + f.mtmarks + f.scmarks) + "\n Percenatge: " +
+                            str(((f.csmarks + f.mtmarks + f.scmarks)/300)*100))
 
                     msg = f'Subject:{subject}\n\n{body}'
                     smtp.sendmail('alokdas9626@gmail,com', emaildb, msg)
-                return render_template('result.html', f=f, d=d, msg="Result has been Send to your given respected Email Id")
+                return render_template('result.html', f=f, d=d,
+                                       msg="Result has been Send to your given respected Email Id")
         return render_template("email.html", msg="Wrong OTP!........Please Login Again")
         # return render_template('email.html')
 
 
-@app.route('/download/<int:d>')
+@app.route('/download/<int:d>',methods=['GET','POST'])
 def download(d):
     f = Student.query.get(d)
     rendered = render_template('result.html', f=f, msg="Result has been Send to your given respected Email Id")
@@ -162,7 +167,6 @@ def add():
             db.session.add(stu)
             db.session.commit()
             flash(f"Data inserted successfully ", "info")
-
         page = request.args.get('page', 1, type=int)
         alldata1 = Student.query.order_by(Student.stuid.asc()).paginate(page=int(page), per_page=5)
         return render_template('index1.html', alldata1=alldata1)
